01f494402bf69d81fa5e4c22de3357a7b921545c
Velaga Bhargav <85831977+bhargav11000@users.noreply.github.com>
2026-06-23 19:59:42 +0530

    Add: two-integer-sum - submission-5

---
+++ Data Structures & Algorithms/two-integer-sum/submission-5.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        store = {}
+
+        for i,n in enumerate(nums):
+            d = target-n
+            if(d in store):
+                return [store[d],i]
+            store[n] = i
